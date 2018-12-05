d5c82bef50604ead448b5122036538b07e286be6
Leo Faucher <leo.faucher@edu.itescia.fr>
2018-12-05 12:51:30 +0100

    premier commit

---
+++ chess.js
@@ -0,0 +1,319 @@
+var chessboard = {
+    ligne1: {
+        case1: {
+            piece: '<i class="fas fa-chess-rook"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black'
+        },
+        case2: {
+            piece: '<i class="fas fa-chess-knight"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black'
+        },
+        case3: {
+            piece: '<i class="fas fa-chess-bishop"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black' 
+        },
+        case4: {
+            piece: '<i class="fas fa-chess-queen"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black'
+        },
+        case5: {
+            piece: '<i class="fas fa-chess-king"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black'
+        },
+        case6: {
+            piece: '<i class="fas fa-chess-bishop"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black'
+        },
+        case7: {
+            piece: '<i class="fas fa-chess-knight"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black'
+        },
+        case8: {
+            piece: '<i class="fas fa-chess-rook"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black'
+        }        
+    },
+    ligne2: {
+        case1: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black'
+        },
+        case2: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black'
+        },
+        case3: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black' 
+        },
+        case4: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black'
+        },
+        case5: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black'
+        },
+        case6: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black'
+        },
+        case7: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'black'
+        },
+        case8: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'black'
+        }
+    },
+    ligne3: {
+        case1: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case2: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case3: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case4: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case5: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case6: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case7: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case8: {
+            piece: '',
+            couleurcase: 'black'
+        }        
+    },
+    ligne4: {
+        case1: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case2: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case3: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case4: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case5: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case6: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case7: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case8: {
+            piece: '',
+            couleurcase: 'white'
+        }     
+    },
+    ligne5: {
+        case1: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case2: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case3: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case4: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case5: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case6: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case7: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case8: {
+            piece: '',
+            couleurcase: 'black'
+        }        
+    },
+    ligne6: {
+        case1: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case2: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case3: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case4: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case5: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case6: {
+            piece: '',
+            couleurcase: 'white'
+        },
+        case7: {
+            piece: '',
+            couleurcase: 'black'
+        },
+        case8: {
+            piece: '',
+            couleurcase: 'white'
+        }  
+    },
+    ligne7: {
+        case1: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white'
+        },
+        case2: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white'
+        },
+        case3: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white' 
+        },
+        case4: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white'
+        },
+        case5: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white'
+        },
+        case6: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white'
+        },
+        case7: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white'
+        },
+        case8: {
+            piece: '<i class="fas fa-chess-pawn"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white'
+        }
+    },
+    ligne8: {
+        case1: {
+            piece: '<i class="fas fa-chess-rook"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white'
+        },
+        case2: {
+            piece: '<i class="fas fa-chess-knight"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white'
+        },
+        case3: {
+            piece: '<i class="fas fa-chess-bishop"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white' 
+        },
+        case4: {
+            piece: '<i class="fas fa-chess-queen"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white'
+        },
+        case5: {
+            piece: '<i class="fas fa-chess-king"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white'
+        },
+        case6: {
+            piece: '<i class="fas fa-chess-bishop"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white'
+        },
+        case7: {
+            piece: '<i class="fas fa-chess-knight"></i>',
+            couleurcase: 'black',
+            couleurpiece: 'white'
+        },
+        case8: {
+            piece: '<i class="fas fa-chess-rook"></i>',
+            couleurcase: 'white',
+            couleurpiece: 'white'
+        }         
+    }
+}
+console.log(chessboard.ligne7.case4);
+
+
+for(let ligne of Object.keys(chessboard)){
+    for(let column of Object.keys(chessboard[ligne])){
+        document.getElementById("chessboardiv").innerHTML += "<div class='whitecase blackpiece'>" + chessboard[ligne][column].piece + "</div>"
+    }
+}
+/*for(let ligne of Object.keys(chessboard)){
+    for(let column of Object.keys(chessboard[ligne])){
+        document.getElementById("chessboardiv").innerHTML = "<div class='blackcase blackpiece'>" + chessboard[ligne][column].piece + "</div>"
+    }
+}*/
